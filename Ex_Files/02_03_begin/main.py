5ef563066f7357ab427af526d5054ef733f0e83c
Garrett Mandrona <gwmandrona@gmail.com>
2024-01-28 22:03:26 -0600

    Finished chapter 2, learned about if/else statements, environment variables, and loops

--- Ex_Files/02_03_begin/main.py
+++ Ex_Files/02_03_begin/main.py
@@ -4,9 +4,10 @@ AGES = [20, 21, 22, 23]
 JOHN = NAMES[0]
 PAUL = NAMES[1]
 
+#Using something here called a slice
 JOHN_PAUL = NAMES[:2]
 GEORGE_RINGO = NAMES[2:]
-REVERSE = NAMES[::-1]
+REVERSE = NAMES[::-1] #Set up as [start:stop:step]
 EVERY_OTHER = NAMES[::2]
 
 print(sum(AGES))
--- Ex_Files/02_04_begin/main.py
+++ Ex_Files/02_04_begin/main.py
@@ -1,6 +1,7 @@
 NAMES = ["John", "Paul", "George", "Ringo"]
 AGES = [20, 21, 22, 23]
 
+#While loop is not as commonly used as a for in loop
 i = 0
 while i < len(NAMES):
     print(NAMES[i], AGES[i])
@@ -10,7 +11,7 @@ for name in NAMES:
     print(name)
 
 for name, age in zip(NAMES, AGES):
-    print(f"{name} {age}")
+    print(f"{name} {age}") #this one is an f string, which basically makes inputting variables easier
 
 for name in reversed(NAMES):
     print(name)
@@ -19,3 +20,5 @@ for i in range(5):
     print(i)
 
 # enumerate
+for i, name in enumerate(NAMES):
+    print(f"{i} {name}")
