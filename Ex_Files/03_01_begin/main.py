3325632d63203c486620a96498f2dc541c565690
Garrett Mandrona <gwmandrona@gmail.com>
2024-01-29 18:19:28 -0600

    Simple commit to start working on desktop

--- Ex_Files/03_01_begin/main.py
+++ Ex_Files/03_01_begin/main.py
@@ -3,6 +3,7 @@ from pprint import pprint
 
 EINSTEIN_CSV = 'Albert,Einstein,1879-03-14,1955-04-18,Germany,"for his services to Theoretical Physics, and especially for his discovery of the law of the photoelectric effect",physics,1921'
 
+#example of a hash table that we will be using a lot
 EINSTEIN = {
     "birthplace": "Germany",
     "name": "Albert",
@@ -11,7 +12,9 @@ EINSTEIN = {
     "category": "physics",
     "motivation": "for his services to Theoretical Physics...",
 }
-
+#'open' is a utility for opening files in general
+#using 'with' means that the file won't be open for too long
+#'r' only means that it stands for reading
 with open("laureates.csv", "r") as f:
     reader = csv.DictReader(f)
     laureates = list(reader)
