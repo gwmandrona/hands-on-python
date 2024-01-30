0ceddd6cb1ce7e8f4ad36ba8e01e118fb3b60841
gmantamu <gwmandrona@gmail.com>
2024-01-29 19:34:23 -0600

    Worked with json manipulation, reading, and transfering from csv format to json format

--- Ex_Files/03_03_begin/main.py
+++ Ex_Files/03_03_begin/main.py
@@ -1,6 +1,6 @@
 import csv
-import json
-from pprint import pprint
+import json #far more popular than csv in modern python development
+from pprint import pprint #pretty print, making outputs improved
 
 EINSTEIN = {
     "birthplace": "Germany",
@@ -11,12 +11,12 @@ EINSTEIN = {
     "motivation": "for his services to Theoretical Physics...",
 }
 
-einstein_json = json.dumps(EINSTEIN)
-back_to_dict = json.loads(einstein_json)
+einstein_json = json.dumps(EINSTEIN) #creaing a string for the json
+back_to_dict = json.loads(einstein_json) #converting the string back to json
 print(einstein_json)
 pprint(back_to_dict)
 
-with open("laureates.csv", "r") as f:
+with open("laureates.csv", "r") as f: #'f' here is the file object
     reader = csv.DictReader(f)
     laureates = list(reader)
 
